e4cdbdc71c8c67986219045ed339187b0b5b3e75
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-06-04 16:14:05 +0530

    mining.js edited

--- mining.js
+++ mining.js
@@ -12,41 +12,44 @@
 // Take the SHA256 hash of the stringified block object
 // Set the resulting value to a hash property on the mined block just before mining it
 
+//3.
+// Now it's time to actually mine the block. This is where we get the work part of proof of work!
+// In the mine function, prior to hashing the block, add a nonce property. This property should start at 0
+// Keep changing the nonce until you find a hash that is less than the TARGET_DIFFICULTY
+
 const SHA256 = require('crypto-js/sha256');
 const TARGET_DIFFICULTY = BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
 const MAX_TRANSACTIONS = 10;
-
 const mempool = [];
 const blocks = [];
 
 function addTransaction(transaction) {
-  
-    mempool.push(transaction)
-
+    mempool.push(transaction);
 }
 
 function mine() {
-    // TODO: mine a block
-    let transactions = []
-    while (transactions.length < MAX_TRANSACTIONS && mempool.length > 0)
-     {
+    let transactions = [];
+    while (transactions.length < MAX_TRANSACTIONS && mempool.length > 0) {
         transactions.push(mempool.pop());
     }
-    
-    
-    const block = {
-        id: blocks.length, transactions
+    const block = { id: blocks.length, transactions }
+    block.nonce = 0;
+    let hash;
+    while (true) {
+        hash = SHA256(JSON.stringify(block)).toString();
+        if (BigInt(`0x${hash}`) < TARGET_DIFFICULTY) {
+            break;
+        }
+        block.nonce++;
     }
-    const hash = SHA256(JSON.stringify(block))
-   
-    blocks.push({...block,hash})
+    blocks.push({ ...block, hash });
 }
 
 module.exports = {
     TARGET_DIFFICULTY,
     MAX_TRANSACTIONS,
-    addTransaction, 
-    mine, 
+    addTransaction,
+    mine,
+    mempool,
     blocks,
-    mempool
 };
