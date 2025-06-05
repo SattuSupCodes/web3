aa0d75bc5db16829a4847e9498983e13b1c4c003
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-06-05 16:07:28 +0530

    blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -2,12 +2,23 @@ const Block = require('./block');
 
 class Blockchain {
     constructor() {
-        this.chain = [ new Block()];
-       
+        this.chain = [new Block()];
     }
-    addBlock(block){
-        block.previousHash = this.chain[this.chain.length - 1 ].toHash() //adding previous hash in new block
-        this.chain.push(block)
+
+    addBlock(block) {
+        block.previousHash = this.chain[this.chain.length - 1].toHash();
+        this.chain.push(block);
+    }
+
+    isValid() {
+        for(let i = this.chain.length - 1; i > 0; i--) { // to validate and compare prev hash to check validity
+            const block = this.chain[i];
+            const prev = this.chain[i - 1];
+            if(block.previousHash.toString() !== prev.toHash().toString()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
