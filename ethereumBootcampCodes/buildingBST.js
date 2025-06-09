4e610694ef5581a96cef84ca95778b8cdd1f9fc7
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-06-09 18:45:15 +0530

    node.js

--- ethereumBootcampCodes/buildingBST.js
+++ ethereumBootcampCodes/buildingBST.js
@@ -53,3 +53,40 @@ class Tree {
 module.exports = Tree;
 
 //4.
+//Complete the function addNode so that it can handle adding nodes no matter how large the tree gets.
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    addNode(node) {
+        if(!this.root) {
+            this.root = node;
+            return;
+        }
+        
+        let ptr = this.root;
+        while(true) {
+            if(node.data < ptr.data) {
+                if(!ptr.left) {
+                    ptr.left = node;
+                    break;
+                }
+                else {
+                    ptr = ptr.left;
+                }
+            }
+            if (node.data > ptr.data) {
+                if (!ptr.right) {
+                    ptr.right = node;
+                    break;
+                }
+                else {
+                    ptr = ptr.right;
+                }
+            }
+        }
+    }
+}
+
+module.exports = Tree;
