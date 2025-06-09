011faffdf3725b913a981bbd04f00d764de2af72
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-06-09 18:49:21 +0530

    node.js

--- ethereumBootcampCodes/buildingBST.js
+++ ethereumBootcampCodes/buildingBST.js
@@ -90,3 +90,60 @@ class Tree {
 }
 
 module.exports = Tree;
+
+//5.
+// Add a method hasNode that will take a number and search our tree to find a node that has that number inside it's data property.
+// If a node exists with the number, return true. If not return false.
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    hasNode(data) {
+        return this.searchRoot(this.root, data)
+    }
+
+    searchRoot(root, data) {
+        if(!root) {
+            return false;
+        }
+        if(root.data === data) {
+            return true;
+        }
+        if(root.data > data) {
+            return this.searchRoot(root.left, data);
+        }
+        if(root.data < data) {
+            return this.searchRoot(root.right, data);
+        }
+    }
+
+    addNode(node) {
+        if(!this.root) {
+            this.root = node;
+        }
+        this.addToRoot(this.root, node);
+    }
+
+    addToRoot(root, node) {
+        if (node.data < root.data) {
+            if (!root.left) {
+                root.left = node;
+            }
+            else {
+                this.addToRoot(root.left, node);
+            }
+        }
+
+        if (node.data > root.data) {
+            if (!root.right) {
+                root.right = node;
+            }
+            else {
+                this.addToRoot(root.right, node);
+            }
+        }
+    }
+}
+
+module.exports = Tree;
