aeb974836315a92307de3461abf39bb105d32de9
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-06-10 18:06:35 +0530

    merkle.js

--- ethereumBootcampCodes/merkleTree.js
+++ ethereumBootcampCodes/merkleTree.js
@@ -28,7 +28,7 @@ class MerkleTree {
     }
     getRoot(leaves = this.leaves) {
         if (leaves.length === 1) {
-            return leaves[0];
+            return leaves[0]; // base case: one item left, that's the root (stoppping condition for our recursion function)
         }
         const layer = [];
         for (let i = 0; i < leaves.length; i += 2) {
@@ -38,6 +38,104 @@ class MerkleTree {
         }
         return this.getRoot(layer);
     }
-}//honestlt, i cheated alot in this merkle tree building. Sorry ahahahahahaha
+}//honestly, i cheated alot in this merkle tree building. Sorry ahahahahahaha
 
 module.exports = MerkleTree;
+
+//3.
+//Your Goal: Handle Odd Number of Leaves
+
+class MerkleTree {
+    constructor(leaves, concat) {
+        this.leaves = leaves;
+        this.concat = concat;
+    }
+    getRoot(leaves = this.leaves) {
+        if (leaves.length === 1) {
+            return leaves[0];
+        }
+        const layer = [];
+        for (let i = 0; i < leaves.length; i += 2) {
+            const left = leaves[i];
+            const right = leaves[i + 1];
+            if (right) {
+                layer.push(this.concat(left, right));
+            }
+            else {
+                layer.push(left);
+            }
+        }
+        return this.getRoot(layer);
+    }
+}
+
+module.exports = MerkleTree;
+
+//4.
+// adding getProof method
+class MerkleTree {
+    constructor(leaves, concat) {
+        this.leaves = leaves;
+        this.concat = concat;
+    }
+    getRoot(leaves = this.leaves) {
+        if (leaves.length === 1) {
+            return leaves[0];
+        }
+        const layer = [];
+        for (let i = 0; i < leaves.length; i += 2) {
+            const left = leaves[i];
+            const right = leaves[i + 1];
+            if (right) {
+                layer.push(this.concat(left, right));
+            }
+            else {
+                layer.push(left);
+            }
+        }
+        return this.getRoot(layer);
+    }
+    getProof(index, layer = this.leaves, proof = []) {
+        if (layer.length === 1) return proof;
+        const newLayer = [];
+        for (let i = 0; i < layer.length; i += 2) {
+            let left = layer[i];
+            let right = layer[i + 1];
+            if (!right) {
+                newLayer.push(left);
+            }
+            else {
+                newLayer.push(this.concat(left, right));
+
+                if (i === index || i === index - 1) {
+                    let isLeft = !(index % 2);
+                    proof.push({
+                        data: isLeft ? right : left,
+                        left: !isLeft
+                    });
+                }
+            }
+        }
+        return this.getProof(Math.floor(index / 2), newLayer, proof);
+    }
+}
+//im being so lazy with this merkle tree sheesh
+module.exports = MerkleTree;
+
+//5.
+//verifying proof
+function verifyProof(proof, node, root, concat) {
+    let data = node;
+    for (let i = 0; i < proof.length; i++) {
+        if (proof[i].left) {
+            data = concat(proof[i].data, data);
+        }
+        else {
+            data = concat(data, proof[i].data);
+        }
+    }
+    return data === root;
+}
+
+module.exports = verifyProof;
+
