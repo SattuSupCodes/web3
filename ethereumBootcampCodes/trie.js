0928c436cd015502671ec38e0236236e133ecb8f
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-06-11 18:32:30 +0530

    trie.js

--- ethereumBootcampCodes/trie.js
+++ ethereumBootcampCodes/trie.js
@@ -18,5 +18,71 @@ class Trie {
         }
     }
 }
+//these trie/trees are a bit ughh ngl
+module.exports = Trie;
+
+//Add branching to the insert function. When a second child comes up, be sure to add it as well.
+
+const TrieNode = require('./TrieNode');
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode(null);
+    }
+    insert(word) {
+        let node = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            if (!node.children[word[i]]) {
+                node.children[word[i]] = new TrieNode(word[i]);
+            }
+
+            node = node.children[word[i]];
+
+            if (i == word.length - 1) {
+                node.isWord = true;
+            }
+        }
+    }
+}
+
+module.exports = Trie;
+
+//adding contains function
+const TrieNode = require('./TrieNode');
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode(null);
+    }
+    insert(word) {
+        let node = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            if (!node.children[word[i]]) {
+                node.children[word[i]] = new TrieNode(word[i]);
+            }
+
+            node = node.children[word[i]];
+
+            if (i == word.length - 1) {
+                node.isWord = true;
+            }
+        }
+    }
+    contains(word) {
+        let node = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            if (node.children[word[i]]) {
+                node = node.children[word[i]];
+            } else {
+                return false;
+            }
+        }
+
+        return node.isWord;
+    }
+}
 
 module.exports = Trie;
