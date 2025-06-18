7e3a8cf6b47fc65bd4c45c7d19633a5231499322
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-06-18 17:00:16 +0530

    get balance api call

---
+++ ethereumBootcampCodes/getBalance.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { API_KEY } = process.env; //api key stayes in .env files (hidden). 
+const axios = require('axios');
+const url = `https://eth-mainnet.g.alchemy.com/v2/${API_KEY}`;
+
+async function getBalance(address) {
+    const response = await axios.post(url, {
+        jsonrpc: "2.0",
+        id: 1,
+        method: "eth_getBalance", 
+        params: [address, "latest"],  
+    });
+
+  
+
+    // TODO: return the balance of the address
+    return response.data.result
+}
+
+module.exports = getBalance;
