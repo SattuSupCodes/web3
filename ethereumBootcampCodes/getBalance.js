797d5e9cea2b86bbb5884040d82083691f5377c4
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-06-18 17:38:21 +0530

    get balance api call

--- ethereumBootcampCodes/getBalance.js
+++ ethereumBootcampCodes/getBalance.js
@@ -18,3 +18,27 @@ async function getBalance(address) {
 }
 
 module.exports = getBalance;
+//When sending transactions in Ethereum it's a requirement to include a nonce. 
+// The reasoning is quite simple: you wouldn't want your transaction to be executed multiple times
+
+//soooo.... for that we can use method : eth_getTransactionCount
+
+require('dotenv').config();
+const { API_KEY } = process.env;
+const axios = require('axios');
+const url = `https://eth-mainnet.g.alchemy.com/v2/${API_KEY}`;
+
+async function getTotalBalance(addresses) {
+    const batch = addresses.map((addr, i) => ({
+        jsonrpc: "2.0",
+        id: i,
+        method: "eth_getBalance",
+        params: [addr],
+    }));
+
+    const response = await axios.post(url, batch);
+
+    return response.data.reduce((p,c) => p + parseInt(c.result), 0);
+}
+
+module.exports = getTotalBalance; //remembinder to me to chatgpt this code cause i don get it fully
