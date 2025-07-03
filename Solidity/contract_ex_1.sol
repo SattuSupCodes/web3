22de27d7d6c22a82572bb79dd49898bea3f601e9
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-07-03 19:58:51 +0530

    upd

---
+++ Solidity/contract_ex_1.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+// import "forge-std/console.sol" 
+contract A{
+    address b;
+    constructor(address _b){
+        b = _b;
+        // console.log(b);
+    }
+}
+
+contract B{}
